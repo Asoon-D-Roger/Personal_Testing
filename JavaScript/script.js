8412e8779a916494a94c241721fe8ae77547558a
“[Asoon-D-Roger]” <[asoon1067@gmail.com]>
2024-04-12 13:02:13 +0530

    self learning

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+ // This is an alert
+        //Single line comment
+        /*
+        Multi  line comment
+        */
+        // alert("Hi")
+        
+        //Variables
+        let score = 0
+        
+        score = 10
+        console.log("score is " + score)
+
+
+        let amount = 1000
+        console.log("Your bill amount is Rs." + amount)
+
+        const pi = 3.14
